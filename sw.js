193683c9ae6caa5d5853a4fcccb2798775fd190a
islamiclibraryy <shekhpodcast@gmail.com>
2026-06-15 20:18:30 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'ledger-premium-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.webp',
+  './icon-512.webp'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets (Offline Support)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
