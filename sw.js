8f72d6716ab76f18254cb54df2e8eb770a32c1f3
islamiclibraryy <shekhpodcast@gmail.com>
2026-06-17 04:14:35 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = 'ledger-premium-v2';
+const CACHE_NAME = 'ledger-premium-v2'; 
 const ASSETS = [
+  './',
   './index.html',
   './manifest.json',
   './icon-192.webp',
@@ -8,10 +9,27 @@ const ASSETS = [
 
 // Install Service Worker
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
     })
+  );
+});
+
+// Activate & Clean Old Caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Old cache deleted:', cache);
+            return caches.delete(cache); 
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
